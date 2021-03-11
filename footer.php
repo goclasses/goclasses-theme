3d2eeb875d21bd32fdb3d60dbe8c6b45764d6640
Erick Santos <ericksantos@alunos.utfpr.edu.br>
2021-03-11 13:20:45 -0300

    Adicionando footer

--- footer.php
+++ footer.php
@@ -1,4 +1,71 @@
+<footer class="footer">
+  <div class="footer-fundo">
+    <div class="container">  
 
+      <div class="logos-footer grid-4">
+        <div class="goclasses-footer-logo">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/logo_nome.svg" alt="logo goclasses">
+        </div>
+        <div class="utfpr-footer-logo">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/utfpr.png" alt="logo utfpr">
+        </div>
+        <div class="byncsa-footer">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/by-nc-sa.svg" alt="by-nc-sa svg">
+        </div>
+      </div>
+
+      <div class="links-uteis grid-5">
+        <h2>links úteis</h2>
+        <div class="links-foooter">
+          <ul>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+            <li><a href="#">https://ipsum.com</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="contato-footer grid-3">
+        <h2>contato</h2>
+
+        <p>email@utfpr.edu.br</p>
+        <p>+55 45 99999-9999</p>
+        <p>Sala X - Bloco Y</p>
+
+        <div class="links-redes-contato">
+          <div class="github-redes">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/github.svg" alt="github logo">
+          </div>
+          <div class="instagram-redes">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg" alt="instagram logo">
+          </div>
+          <div class="facebook-redes">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/facebook.svg" alt="facebook logo">
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="copy">
+    <div class="container">
+      <p class="grid-12">GoClasses&copy;<?php echo date('Y'); ?> - Alguns direitos reservados.</p>
+    </div>
+  </div>
+
+</footer>
 
 
   <?php wp_footer(); ?>
