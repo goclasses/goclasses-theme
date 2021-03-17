9b041f3fd33366cf962f4ed8cf0dcf1537f7785c
Erick Santos <ericksantos@alunos.utfpr.edu.br>
2021-03-17 19:04:40 -0300

    Adicionando a página contato

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/inicio.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/quemsomos.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/contato.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
   <!-- JS -->
@@ -22,11 +23,11 @@
 </head>
 
   <header class="header">
-      <div class="logo"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/logo_nome.svg" alt="Logo GoClasses"></a></div>
+      <div class="logo"><a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logo_nome.svg" alt="Logo GoClasses"></a></div>
       <nav class="navegacao_header">
         <ul>
-          <li class="current-menu-item"><a href="#">Quem Somos</a></li>
-          <li><a href="#">Projetos</a></li>
+          <li class="current-menu-item"><a href="/quemsomos">Quem Somos</a></li>
+          <li><a href="/projetos">Projetos</a></li>
           <li class="parent">
             <a href="#">Materiais didáticos</a>
             <ul>
@@ -35,7 +36,7 @@
               <li><a href="#">Programação Paralela e Distribuída</a></li>
             </ul>
           </li>
-          <li><a href="#">Contato</a></li>
+          <li><a href="/contato">Contato</a></li>
           <li><a href="#">Entrar</a></li>
         </ul>
       </nav>
@@ -45,7 +46,7 @@
         <i class="fa fa-close"></i>
       </label>
       <div class="search-box">
-        <form>
+        <form class="form-search">
           <input type="search" placeholder="Pesquisar" class="search" required>
           <input type="submit" value="IR" class="sub">
         </form>
---
+++ page-contato.php
@@ -0,0 +1,57 @@
+<?php 
+  // Template Name: Contato
+  get_header(); 
+?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<!-- Começa código da página -->
+
+<section class="cabecalho">
+  <div>
+    <h1><?php the_title(); ?></h1>
+  </div>
+</section>
+
+<section class="contato container">
+  <form action="" id="form_contato" method="POST" class="contato_form grid-6 formphp">
+      <label for="nome">Nome</label>
+      <input type="text" id="nome" name="nome" required>
+      <label for="disciplina">Disciplina</label>
+      <input type="text" id="disciplina" name="disciplina" required>
+      <label for="email">E-mail</label>
+      <input type="email" id="email" name="email" required>
+      <label for="mensagem">Mensagem</label>
+      <textarea id="mensagem" name="mensagem" required></textarea>
+      <button id="enviar" name="enviar" type="submit" class="botao-red-cinza">Enviar</button>
+  </form>
+
+  <div class="grid-6 contato-info">
+    <h3>Horário de Atendimento</h3>
+    <p>seg, qua e sex das 12h  às 17h</p>
+    <p>ter e qui das 17h  às 22h</p>
+    <p>sab das 13h  às 15h</p>
+
+    <h3 class="siga">Siga nossas redes</h3>
+    <div class="links-redes-contato">
+      <div class="github-redes">
+        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/github.svg" alt="github logo"></a>
+      </div>
+      <div class="instagram-redes">
+        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg" alt="instagram logo"></a>
+      </div>
+      <div class="facebook-redes">
+        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook.svg" alt="facebook logo"></a>
+      </div>
+    </div>
+
+  </div>
+
+</section>
+
+<!-- Fecha while -->
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
