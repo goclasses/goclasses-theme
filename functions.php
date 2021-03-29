d5901ece4971f97451453324bf75fe06c9dbc6d1
Erick Santos <ericksantos@alunos.utfpr.edu.br>
2021-03-29 11:39:47 -0300

    CSS da páginade login

--- functions.php
+++ functions.php
@@ -15,3 +15,18 @@ remove_action('admin_print_styles', 'print_emoji_styles');
 // Função para desabilitar o editor de blocos
 add_filter('use_block_editor_for_post', '__return_false', 10);
 
+//Função para alterar o CSS da página logim
+function my_custom_login_stylesheet() {
+  wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style.css' );
+}
+add_action( 'login_enqueue_scripts', 'my_custom_login_stylesheet' );
+/*Função que altera a URL, trocando pelo endereço do seu site*/
+function my_login_logo_url() {
+  return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+/*Função que adiciona o nome do seu site, no momento que o mouse passa por cima da logo*/
+function my_login_logo_url_title() {
+return 'GoClasses - Voltar para Home';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
