51954608f17ba48f466d7cc9ab2b87f6762cecb1
Erick Santos <ericksantos@alunos.utfpr.edu.br>
2021-04-05 15:55:21 -0300

    Criando a página index.php para servir de archive para os post do blog

--- index.php
+++ index.php
@@ -1,12 +1,38 @@
-<?php get_header(); ?> 
+<?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
-	<?php the_title(); ?>
-	<?php the_content(); ?>
+<!-- Começa código da página -->
 
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
+<section class="cabecalho-index">
+  <div>
+    <h1>blog glocasses</h1>
+  </div>
+</section> 
+
+<!-- post  -->
+<div class="container post-page">
+
+
+	<?php while ( have_posts() ) {
+		the_post(); 
+	?>
+
+		<div class="projeto-ind grid-4">
+			<div class="imagem-projeto">
+				<a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/imagem-pc.jpg" alt="Post <?php the_title(); ?>"></a>
+			</div>
+			<div class="projeto-info">
+				<h3><?php the_title(); ?></h3>
+				<p class="info-projeto-ind"><?php the_author_meta( 'nicename'); ?> - <?php the_date('d/m/Y'); ?></p>
+				<p class="sobre-projeto-ind"><?php echo wp_trim_words(get_the_content(), 12); ?></p>
+				<a href="<?php the_permalink(); ?>" class="botao-red-cinza">abrir</a>
+			</div>
+		</div>
+
+	<?php } ?>
+
+
+</div>
+
+<!-- Fecha while -->
 
 <?php get_footer(); ?>
