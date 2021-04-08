1522c070737a6d6be08790027e478b94d051d7e7
Erick Santos <ericksantos@alunos.utfpr.edu.br>
2021-04-07 22:33:31 -0300

    Adicionando posts para o blog

--- index.php
+++ index.php
@@ -9,27 +9,38 @@
 </section> 
 
 <!-- post  -->
-<div class="container post-page">
+<div class="container">
 
+	<div class="post-page">
 
-	<?php while ( have_posts() ) {
-		the_post(); 
-	?>
+		<?php while ( have_posts() ) {
+			the_post(); 
+		?>
 
-		<div class="projeto-ind grid-4">
-			<div class="imagem-projeto">
-				<a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/imagem-pc.jpg" alt="Post <?php the_title(); ?>"></a>
+			<div class="grid-4">
+				<div class="projeto-ind">
+					<div class="imagem-projeto">
+						<a href="<?php the_permalink(); ?>"><img src="<?php 
+							$value = get_field('imagem');
+							if( $value ) {
+								echo $value;
+							} else {
+								echo get_template_directory_uri() . '/img/imagem-pc.jpg';
+							}
+						?>" alt="Post <?php the_title(); ?>"></a>
+					</div>
+					<div class="projeto-info">
+						<h3><?php the_title(); ?></h3>
+						<p class="info-projeto-ind"><?php the_author_meta( 'nicename'); ?> - <?php the_time('d/m/Y'); ?></p>
+						<p class="sobre-projeto-ind"><?php echo wp_trim_words(get_the_content(), 12); ?></p>
+						<a href="<?php the_permalink(); ?>" class="botao-red-cinza">abrir</a>
+					</div>
+				</div>
 			</div>
-			<div class="projeto-info">
-				<h3><?php the_title(); ?></h3>
-				<p class="info-projeto-ind"><?php the_author_meta( 'nicename'); ?> - <?php the_date('d/m/Y'); ?></p>
-				<p class="sobre-projeto-ind"><?php echo wp_trim_words(get_the_content(), 12); ?></p>
-				<a href="<?php the_permalink(); ?>" class="botao-red-cinza">abrir</a>
-			</div>
-		</div>
 
-	<?php } ?>
+		<?php } ?>
 
+	</div>
 
 </div>
 
