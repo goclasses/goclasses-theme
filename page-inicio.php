8f6262157aa2199920569712ddb7e3b5eef41e88
oRander <rander@utfpr.edu.br>
2021-05-09 00:43:57 -0300

    Adicionado um custom field na tela inicial em projeto

--- page-inicio.php
+++ page-inicio.php
@@ -72,7 +72,7 @@
   </div>
   <div class="grid-5">
     <h2>O projeto...</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <?php the_field('o_projeto', $inicio); ?>
   </div>
 </section>
 <!-- Sobre o projeto termina -->
