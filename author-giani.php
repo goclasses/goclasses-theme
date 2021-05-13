5885c52db1fed9bccc02a97f695671d8c6a41010
Erick Santos <ericksantos@alunos.utfpr.edu.br>
2021-05-12 22:44:48 -0300

    Colocando informações pessoais e de contato gerenciaveis

--- author-giani.php
+++ author-giani.php
@@ -1,4 +1,9 @@
-<?php get_header(); ?>
+<?php 
+  $id_pagina = get_page_by_title('giani');
+  $instagram = get_field('instagram', $id_pagina);
+  $facebook = get_field('facebook', $id_pagina);
+  get_header(); 
+?>
 
 <!-- Começa código da página -->
 
@@ -13,28 +18,28 @@
     <div class="container">
       <div class="giani-info">
         <div class="grid-6">
-          <h1>Giani Carla Ito</h1>    
-          <p class="area-atuacao">Doutora em Computação Aplicada</p>
+          <h1><?php the_field('nome_completo', $id_pagina) ?></h1>    
+          <p class="area-atuacao"><?php the_field('area_de_atuacao', $id_pagina); ?></p>
           <ul>
-            <a href="mailto:gianiito@gmail.com" target="_blank">
+            <a href="mailto:<?php the_field('email', $id_pagina); ?>" target="_blank">
               <li>
                 <div class="social_logo"><img src="<?php echo get_template_directory_uri(); ?>/img/info-email.png" alt=""></div>
-                <p>gianiito@utfpr.edu.br</p>
+                <p><?php the_field('email', $id_pagina); ?></p>
               </li>
             </a>
-            <a href="#" target="_blank">
+            <a href="<?php echo $instagram['link_instagram']; ?>" target="_blank">
               <li>
                 <div class="social_logo"><img src="<?php echo get_template_directory_uri(); ?>/img/info-insta.png" alt=""></div>
-                <p>@itogiani</p>
+                <p>@<?php echo $instagram['usuario_instagram']; ?></p>
               </li>
             </a>
-            <a href="#" target="_blank">
+            <a href="<?php echo $facebook['link_facebook']; ?>" target="_blank">
               <li>
                 <div class="social_logo"><img src="<?php echo get_template_directory_uri(); ?>/img/info-facebook.png" alt=""></div>
-                <p>/giani.ito</p>
+                <p>/<?php echo $facebook['usuario_facebook']; ?></p>
               </li>
             </a>
-            <a href="#" target="_blank">
+            <a href="<?php the_field('lattes', $id_pagina); ?>" target="_blank">
               <li>
                 <div class="social_logo"><img src="<?php echo get_template_directory_uri(); ?>/img/info-lattes.png" alt=""></div>
                 <p>Lattes</p>
@@ -44,13 +49,13 @@
         </div>
         <div class="grid-6">
           <div class="imagem_giani">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/giani.jpg" alt="Foto Giani">
+            <img src="<?php the_field('foto_de_perfil', $id_pagina); ?>" alt="Foto Giani">
           </div>
         </div>
       </div>
       <div class="resumo_giani">
         <div class="grid-12">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p><?php the_field('resumo_sobre', $id_pagina); ?></p>
         </div>
       </div>
       <div class="carreira_giani">
