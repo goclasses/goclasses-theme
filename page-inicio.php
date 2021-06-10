6c7ffc7b744b53485f0e54d2f35db0bb94dbd7fd
Erick Santos <erickosantos.dev@gmail.com>
2021-06-10 16:44:06 -0300

    Arrumando pagina inicial

--- page-inicio.php
+++ page-inicio.php
@@ -44,16 +44,11 @@
             <a href="<?php the_permalink(); ?>">
               <img src="<?php 
                 $value = get_field('icone_materia');
-                if( $value ) {
-                  echo $value;
-                } else {
-                  echo get_template_directory_uri() . '/img/caderno.svg';
-                }
-              ?>" alt="Ícone da Matéria <?php the_title(); ?>">
+                echo $value;
+              ?>" alt="Imagem da Matéria <?php the_title(); ?>">
             </a>
           </div>
           <h3><?php the_title(); ?></h3>
-          <a href="<?php the_permalink(); ?>" class="botao-red">Saiba mais</a>
         </div>
       </div>
 
@@ -63,60 +58,48 @@
 </section>
 <!-- Materiais termina -->
 
-<!-- Ultimos projetos e posts começa -->
-<section class="container ultimos">
-  <div class="ultimos-posts grid-6">
-    <h2>últimos posts</h2>
-
-    <?php
-    $homepagePosts = new WP_Query(array(
-      'posts_per_page' => 3,
-      'post_type' => 'post'
-    ));
-
-    while($homepagePosts->have_posts()) {
-      $homepagePosts->the_post(); ?>
-
-      <div class="ultimo-po-individual">
-        <h3><?php the_title(); ?></h3>
-        <p class="ultimo-post-ind-info"><?php the_time('d/m/Y'); ?></p>
-        <p class="ultimo-post-ind-desc">
-          <?php echo wp_trim_words(get_the_content(), 12); ?>
-        </p>
-        <a href="<?php the_permalink(); ?>" class="botao-red-cinza">leia mais</a>
+<!-- Pessoas destaques -->
+<section class="materiais-inicio pessoas-destaque-container">
+  <h2>ALUNOS DESTAQUES</h2>
+  <div class="container materiais-3">
+    <div class="grid-4">
+      <div class="pessoa-destaque">
+        <div class="material-individual pessoa-destaque-box">
+          <div class="pessoa-destaque-img">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/perfil-erick.png" alt="">
+          </div>
+          <p class="destaque-nome">Erick santos</p>
+          <p class="destaque-disciplina">Linguagem de Programação Estruturada</p>
+        </div>
       </div>
-
-    <?php } ?>
-
-  </div>
-
-  <div class="ultimos-projetos grid-6">
-    <h2>últimos projetos</h2>
-
-    <?php
-    $homepageProjetos = new WP_Query(array(
-      'posts_per_page' => 3,
-      'post_type' => 'projeto'
-    ));
-
-    while($homepageProjetos->have_posts()) {
-      $homepageProjetos->the_post(); ?>
-
-    <div class="ultimo-po-individual">
-      <h3><?php the_title(); ?></h3>
-      <p class="ultimo-post-ind-info"><?php the_author_meta( 'nicename'); ?> - <?php the_time('d/m/Y'); ?></p>
-      <p class="ultimo-post-ind-desc">
-        <?php echo wp_trim_words(get_the_content(), 12); ?>
-      </p>
-      <a href="<?php the_permalink(); ?>" class="botao-red-cinza">leia mais</a>
     </div>
 
-    <?php } ?>
+    <div class="grid-4">
+      <div class="pessoa-destaque">
+        <div class="material-individual pessoa-destaque-box">  
+          <div class="pessoa-destaque-img">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/perfil-Giani.png" alt="">
+          </div>
+          <p class="destaque-nome">Giani Carla Ito</p>
+          <p class="destaque-disciplina">Comunicação de Dados</p>
+        </div>
+      </div>
+    </div>
 
+    <div class="grid-4">
+      <div class="pessoa-destaque">
+        <div class="material-individual pessoa-destaque-box">
+          <div class="pessoa-destaque-img">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/perfil-rander.png" alt="">
+            <p class="destaque-nome">Rander Carneiro</p>
+            <p class="destaque-disciplina">Programação Paralela e Distribuída</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
-<!-- Ultimos projetos e posts termina -->
-
+<!-- Pessoas destaques -->
 
 <!-- JavaScript -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/simple-slide.js"></script>
