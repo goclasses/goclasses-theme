fd6174a44b84a2639f2e2d451f0cad011ce5f5b9
Erick Santos <erickosantos.dev@gmail.com>
2021-06-14 10:45:12 -0300

    Adicionando botão de próximo e anterior

--- single-material.php
+++ single-material.php
@@ -14,17 +14,19 @@
 
         <div class="single-post-content">
           <?php the_content(); ?>
+          <?php if(get_field('material_anterior')) { ?>
+            <a href="<?php the_field('material_anterior'); ?>" class="botao-red-cinza botao_materiais">Material Anterior</a>
+          <?php } ?>
+          <?php if(get_field('proximo_material')) { ?>
+            <a href="<?php the_field('proximo_material'); ?>" class="botao-red-cinza botao_materiais">Próximo Material</a>
+          <?php } ?>
         </div>
 
       <?php } ?>
 
-      <?php
-
-          if(get_current_user_id() == get_the_author_meta('id')) { ?>
-            <a href="<?php echo get_edit_post_link(); ?>" class="edit_button">EDITAR</a>
-          <?php }
-
-        ?>
+      <?php if(get_current_user_id() == get_the_author_meta('id')) { ?>
+        <a href="<?php echo get_edit_post_link(); ?>" class="edit_button">EDITAR</a>
+      <?php } ?>
     </div>
   </div>
 
