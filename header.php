ac500242ddf26863bf3f646e250f4d2c823d6627
Erick Santos <erickosantos.dev@gmail.com>
2021-06-23 00:05:10 -0300

    Adicionando google analytics

--- header.php
+++ header.php
@@ -10,6 +10,15 @@
   <!-- JS -->
   <script>document.documentElement.classList.add("js");</script>
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-7HH97EF2VJ"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-7HH97EF2VJ');
+  </script>
 </head>
 
 <body>
